33863d0ad8617b352c5cf1f165778aae15ba7b19
Aleksandar.Panayotov@a1.bg <Aleksandar.Panayotov@a1.bg>
2023-03-07 13:55:39 +0200

    demo

--- Mentoring/Program.cs
+++ Mentoring/Program.cs
@@ -7,7 +7,6 @@ namespace Mentoring
     {
         static void Main(string[] args)
         {
-            KatalarNumbers();
         }
 
         public static void WriteToFile(string filePath)
@@ -30,18 +29,5 @@ namespace Mentoring
             return readText;
         }
 
-        public static void KatalarNumbers()
-        {
-            Console.Write("Enter N: (N >=0 ) ");
-            int n = Int32.Parse(Console.ReadLine());
-
-            int fact2N = 2 * n, factNplus1 = n + 1;
-
-            for (int i = fact2N - 1; i > 0; i--) fact2N *= i;
-            for (int i = factNplus1 - 1; i > 0; i--) factNplus1 *= i;
-            for (int i = n - 1; i > 0; i--) n *= i;
-
-            Console.WriteLine("Result is {0}", fact2N / (factNplus1 * n));
-        }
     }
 }
